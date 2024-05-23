09ebab800a80654c4355170bbc08c0d7010db5a7
donghyun <kdhdh001@naver.com>
2024-05-23 22:29:57 +0900

    [feat] Team 전체 조회 구현

--- src/main/java/com/group/commutingsystemapp/controller/team/TeamController.java
+++ src/main/java/com/group/commutingsystemapp/controller/team/TeamController.java
@@ -1,8 +1,11 @@
 package com.group.commutingsystemapp.controller.team;
 
 import com.group.commutingsystemapp.domain.team.Team;
+import com.group.commutingsystemapp.dto.TeamDTO;
 import com.group.commutingsystemapp.service.team.TeamService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +21,10 @@ public class TeamController {
     @PostMapping
     public Team createTeam(@RequestBody Team team) {
         return teamService.saveTeam(team);
+    }
+
+    @GetMapping
+    public List<TeamDTO> getAllTeams() {
+        return teamService.getAllTeams();
     }
 }
--- src/main/java/com/group/commutingsystemapp/service/team/TeamService.java
+++ src/main/java/com/group/commutingsystemapp/service/team/TeamService.java
@@ -2,7 +2,9 @@ package com.group.commutingsystemapp.service.team;
 
 import com.group.commutingsystemapp.domain.team.Team;
 import com.group.commutingsystemapp.domain.team.TeamRepository;
+import com.group.commutingsystemapp.dto.TeamDTO;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,15 @@ public class TeamService {
 
     public Team saveTeam(Team team) {
         return teamRepository.save(team);
+    }
+
+    public List<TeamDTO> getAllTeams() {
+        return teamRepository.findAll().stream()
+                .map(team -> new TeamDTO(
+                        team.getName(),
+                        team.getManager() != null ? team.getManager().getName() : null,
+                        team.getMembers().size()
+                ))
+                .collect(Collectors.toList());
     }
 }
