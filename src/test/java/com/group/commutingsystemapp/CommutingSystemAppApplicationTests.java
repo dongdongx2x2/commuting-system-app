51d9244f2bf104d46edaed48b3ef5a5acf3df270
donghyun <kdhdh001@naver.com>
2024-05-23 15:23:06 +0900

    [feat] spring intializer

---
+++ src/test/java/com/group/commutingsystemapp/CommutingSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.commutingsystemapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommutingSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
